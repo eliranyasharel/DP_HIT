eeb94f0b998738514f053594b7cf5602ca5f3f79
KobiHason <hasonkobi@gmail.com>
2018-07-20 11:01:48 +0300

    Add files via upload
    Main idea: 2 feature for or facebook friend's list 
    
    * filter my friend's lists by user religion and show me only  Jewish V Muslim V Christian friend's 
    * filter my friend's lists and show me only the  single V married  V in relationship friend's 
    
    ## ScreenShot 
    ![ScreenShot]()

---
+++ facebookApi/facebookApi/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FacebookWrapper.ObjectModel;
+
+namespace facebookApi
+{
+    public partial class Form1 : Form 
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Single_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox7_CheckedChanged(object sender, EventArgs e)
+        {
+
+          bool men = true;
+
+
+        }
+
+        private void checkBox1_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            bool woman = true;
+        }
+    }
+}
---
+++ facebookApi/facebookApi/Logic/UserRelationshipStatus.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FacebookWrapper.ObjectModel;
+
+
+namespace facebookApi.Logic
+{
+    public class UserRelationshipStatus : FacebookUser
+    {
+
+        private User m_User;
+
+
+        public string GetUserRelationshipStatus()
+        {
+            string relation_status = null;
+            relation_status = m_User.RelationshipStatus.ToString();
+            return relation_status;
+
+        }
+
+
+
+
+
+
+    }
+}
